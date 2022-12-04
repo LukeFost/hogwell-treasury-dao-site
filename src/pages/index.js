78c314e2a2a0910400d30c10bf144888687335dc
SupremeSeymour <robyteoneoneone@gmail.com>
2022-12-03 18:31:14 -0700

    cahnges

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,17 +11,18 @@ export default (() => {
 			<title>
 				Business Matchmaking Has Never Been Easier
 			</title>
-			<meta name={"description"} content={"Manage all your events in a single app. Our online event management platform provides various services event planners and project managers may need."} />
-			<meta property={"og:title"} content={"Business Matchmaking Has Never Been Easier"} />
-			<meta property={"og:description"} content={"Manage all your events in a single app. Our online event management platform provides various services event planners and project managers may need."} />
-			<meta property={"og:image"} content={"https://uploads.quarkly.io/6143a05129a0a1001e6c835c/images/OG-image.png?v=2021-09-22T11:33:13.239Z"} />
-			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/6143a05129a0a1001e6c835c/images/favicon%2032x32.png?v=2021-09-22T11:33:41.498Z"} type={"image/x-icon"} />
-			<link rel={"apple-touch-icon"} href={"https://uploads.quarkly.io/6143a05129a0a1001e6c835c/images/152x152.png?v=2021-09-22T11:33:48.679Z"} />
-			<link rel={"apple-touch-icon"} sizes={"76x76"} href={"https://uploads.quarkly.io/6143a05129a0a1001e6c835c/images/152x152.png?v=2021-09-22T11:33:48.679Z"} />
-			<link rel={"apple-touch-icon"} sizes={"152x152"} href={"https://uploads.quarkly.io/6143a05129a0a1001e6c835c/images/152x152.png?v=2021-09-22T11:33:48.679Z"} />
-			<link rel={"apple-touch-startup-image"} href={"https://uploads.quarkly.io/6143a05129a0a1001e6c835c/images/152x152.png?v=2021-09-22T11:33:48.679Z"} />
-			<meta name={"msapplication-TileImage"} content={"https://uploads.quarkly.io/6143a05129a0a1001e6c835c/images/270x270.png?v=2021-09-22T11:33:55.934Z"} />
+			<meta name={"description"} content={"Hogwell is a DAO-based fund focused on simple straighforward economics and principles."} />
+			<meta property={"og:title"} content={"Hogwell"} />
+			<meta property={"og:description"} content={"Hogwell is a DAO-based fund focused on simple straighforward economics and principles."} />
+			<meta property={"og:image"} content={"https://gateway.pinata.cloud/ipfs/QmWHypqAKJ6zsDENjoCTCRrPh475ht83Q6SYNC4W3qyNS8"} />
+			<link rel={"shortcut icon"} href={"https://gateway.pinata.cloud/ipfs/QmWHypqAKJ6zsDENjoCTCRrPh475ht83Q6SYNC4W3qyNS8"} type={"image/x-icon"} />
+			<link rel={"apple-touch-icon"} href={"https://gateway.pinata.cloud/ipfs/QmWHypqAKJ6zsDENjoCTCRrPh475ht83Q6SYNC4W3qyNS8"} />
+			<link rel={"apple-touch-icon"} sizes={"76x76"} href={"https://gateway.pinata.cloud/ipfs/QmWHypqAKJ6zsDENjoCTCRrPh475ht83Q6SYNC4W3qyNS8"} />
+			<link rel={"apple-touch-icon"} sizes={"152x152"} href={"https://gateway.pinata.cloud/ipfs/QmWHypqAKJ6zsDENjoCTCRrPh475ht83Q6SYNC4W3qyNS8"} />
+			<link rel={"apple-touch-startup-image"} href={"https://gateway.pinata.cloud/ipfs/QmWHypqAKJ6zsDENjoCTCRrPh475ht83Q6SYNC4W3qyNS8"} />
+			<meta name={"msapplication-TileImage"} content={"https://gateway.pinata.cloud/ipfs/QmWHypqAKJ6zsDENjoCTCRrPh475ht83Q6SYNC4W3qyNS8"} />
 		</Helmet>
+		{/*
 		<Section background="--color-lightD1">
 			<Text margin="0px 0 24px 0" text-align="center" font="--headline2" md-font="--headline3">
 				Hogwell Treasury DAO
@@ -40,10 +41,11 @@ export default (() => {
 				<Override slot="link-active" color="--primary" />
 				<Override slot="item" padding="6px" />
 			</Menu>
-		</Section>
+		</Section> 
+*/}
 		<Section
 			padding="40px 0 40px 0"
-			background="linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(https://uploads.quarkly.io/6386ff7f855d39001fcded51/images/Skye_retro_wave_style_ufo_cult_members_are_older_and_are_in_pas_06456fb9-0886-40b9-8026-655787e756b4.png?v=2022-11-30T07:47:17.373Z) center center/cover no-repeat"
+			background="linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(https://gateway.pinata.cloud/ipfs/Qmbg5P76RBPHj48hjurRpfux8HL3VqHzkyZmHX4esNTJFu) center center/cover no-repeat"
 			min-height="100vh"
 			lg-padding="80px 0 80px 0"
 			md-padding="96px 0 96px 0"
@@ -93,7 +95,7 @@ export default (() => {
 			<Box margin="-16px -16px -16px -16px" padding="16px 16px 16px 16px" display="flex" flex-wrap="wrap">
 				<Box padding="16px 16px 16px 16px" width="50%" lg-width="100%">
 					<Box
-						background="url(https://uploads.quarkly.io/6386ff7f855d39001fcded51/images/Cool_Guy__The_beginning_the_middle_and_the_end_of_time_space_an_8bfadb8a-9682-4a56-94b0-28769b0c170a.png?v=2022-11-30T07:37:35.419Z) 50% 15%/cover"
+						background="url(https://gateway.pinata.cloud/ipfs/Qmd4Kkbo4BRsipy3JbCLLwz4pHqcEnC4o83w8Mg4T63Z6c) 50% 15%/cover"
 						padding="0px 0px 672px 0px"
 						margin="0px -112px 0px 0px"
 						lg-margin="0px 0px 0px 0px"
@@ -170,7 +172,7 @@ export default (() => {
 					lg-padding="0px 0px 0px 0px"
 				>
 					<Image
-						src="https://uploads.quarkly.io/6386ff7f855d39001fcded51/images/Cool_Guy__The_cosmic_exsistence_of_a_market_that_intertwines_an_147c455d-2394-414a-a733-337e9ce0dbc1.png?v=2022-11-30T07:51:35.099Z"
+						src="https://gateway.pinata.cloud/ipfs/Qmb21wLcAevkLjr4SmPkMCNbHFXWMQNS38eDKgM2bY1pYv"
 						border-radius="24px"
 						max-width="100%"
 						max-height="456px"
@@ -232,7 +234,7 @@ export default (() => {
 					lg-padding="0px 0px 0px 0px"
 				>
 					<Image
-						src="https://uploads.quarkly.io/6386ff7f855d39001fcded51/images/My%20project%20%281%29.png?v=2022-11-30T08:53:33.512Z"
+						src="https://gateway.pinata.cloud/ipfs/QmfGsjBp3bcm47aTcKhRuQwo7MCZJHAiGUVqBZSXAMjjgz/farmer_lookin.png"
 						border-radius="24px"
 						max-width="100%"
 						max-height="456px"
@@ -296,7 +298,7 @@ export default (() => {
 					lg-padding="0px 0px 0px 0px"
 				>
 					<Image
-						src="https://uploads.quarkly.io/6386ff7f855d39001fcded51/images/koko_maru_Seljuk_emperor_watching_the_ships_approaching_the_har_66e6b8ce-aca1-4514-b119-a1eeae24e15a%20%283%29.png?v=2022-11-30T08:58:09.232Z"
+						src="https://gateway.pinata.cloud/ipfs/QmfGsjBp3bcm47aTcKhRuQwo7MCZJHAiGUVqBZSXAMjjgz/seljwell.png"
 						border-radius="24px"
 						max-width="100%"
 						max-height="456px"
@@ -336,28 +338,6 @@ export default (() => {
 				</Box>
 			</LinkBox>
 		</Section>
-		<Link
-			font={"--capture"}
-			font-size={"10px"}
-			position={"fixed"}
-			bottom={"12px"}
-			right={"12px"}
-			z-index={"4"}
-			border-radius={"4px"}
-			padding={"5px 12px 4px"}
-			background-color={"--dark"}
-			opacity={"0.6"}
-			hover-opacity={"1"}
-			color={"--light"}
-			cursor={"pointer"}
-			transition={"--opacityOut"}
-			quarkly-title={"Badge"}
-			text-decoration-line={"initial"}
-			href={"https://quarkly.io/"}
-			target={"_blank"}
-		>
-			Made on Quarkly
-		</Link>
 		<RawHtml>
 			<style place={"endOfHead"} rawKey={"6143a05129a0a1001e6c835a"}>
 				{":root {\n  box-sizing: border-box;\n}\n\n* {\n  box-sizing: inherit;\n}\n\n* {\n    scroll-behavior: smooth;\n    }"}
